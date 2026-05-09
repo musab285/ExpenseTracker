9a5cc493604355ef32973f95eea9e7b1dbb8ba32
Musab <mmak.lughat@gmail.com>
2026-05-09 19:54:25 +0500

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,8 +98,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
-    # 'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
