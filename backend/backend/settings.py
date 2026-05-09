1fe1e3645c7897c4065ce4427f582d4701617b66
Musab <mmak.lughat@gmail.com>
2026-05-09 20:43:11 +0500

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -155,11 +155,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600,
-        ssl_require=True if not DEBUG else False
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
